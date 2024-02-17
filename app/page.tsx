af9c37796988e2938a17632da2c7601aa45ba370
akuliakuli <kabby889@gmail.com>
2024-02-18 00:35:12 +0400

    okay

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react";
 import jwt from 'jsonwebtoken'
 
@@ -30,22 +31,27 @@ export default function Home() {
 
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <form>
-          <h1>{message}</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#CEF9F2]">
+        <form className="flex flex-col bg-white rounded-md p-3 items-center w-[40%] h-[400px] justify-center">
+          <h1 className="text-3xl mb-[30px]">{message}</h1>
           <input 
               type="text" 
-              name="username" 
+              name="username"
+              placeholder="username"
+              className="text-2xl rounded-md mb-[30px]"
               value={username} 
               onChange={e => setUsername(e.target.value)}
             />
           <input 
               type="password" 
               name="password" 
+              placeholder="password"
+              className="text-2xl rounded-md mb-[30px]"
+
               value={password}
               onChange={e => setPassword(e.target.value)}
             />
-          <input type="submit" value="Login" onClick={submitForm}/>
+          <input type="submit" value="Login" className="rounded-full p-2 text-white bg-blue-700 text-2xl mt-[20px]" onClick={submitForm}/>
         </form>
     </main>
   );
